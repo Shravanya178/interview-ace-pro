d2dd6861080521ce0bcac7c5e631c52c8e65c8ed
khushi-shukla-cz <khushi.j.shukla@gmail.com>
2025-04-16 00:25:42 +0530

    Simplify Python API to eliminate pip3.9 dependency

--- api/index.py
+++ api/index.py
@@ -1,24 +1,27 @@
-from flask import Flask, request, jsonify
+import json
 
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def home():
-    return jsonify({"message": "API is online"})
-
-@app.route('/', methods=['POST'])
-def process():
-    data = request.json
-    return jsonify({"status": "success", "message": "Received data"})
-
-# Handler for Vercel
 def handler(request, context):
-    with app.test_client() as client:
-        response = client.get('/')
+    """
+    Simple serverless function handler
+    """
+    if request.method == "GET":
         return {
             "statusCode": 200,
-            "body": response.get_data(as_text=True),
             "headers": {
                 "Content-Type": "application/json"
-            }
+            },
+            "body": json.dumps({"message": "API is online"})
+        }
+    elif request.method == "POST":
+        try:
+            body = json.loads(request.body)
+        except:
+            body = {}
+            
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({"status": "success", "message": "Received data"})
         } 
